49223b2dffc8bd1880093073bec57327e06598de
Nrnsahebmdi <nooruddin.saheb@mobile-di.com>
2020-01-31 17:25:22 +0530

    auth and user listing after auth has been added next try pagination

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService, AuthenticationService } from '@app/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    users: User[];
+    items = [];
+    pageOfItems: Array<any>;
+
+    constructor(private userService: UserService) { }
+
+    // tslint:disable-next-line:use-lifecycle-interface
+    ngOnInit() {
+        this.loading = true;
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+        
+    }
+
+    onChangePage(pageOfItems: Array<any>) {
+        // update current page of items
+        this.pageOfItems = pageOfItems;
+    }
+}
